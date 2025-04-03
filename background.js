f7b378da880c22612e60da848fcf97588d1ba024
sharafadeen hammed <sharafadeenhammed@yahoo.com>
2025-04-03 12:39:38 +0100

    completed tab signaling to content_script

---
+++ background.js
@@ -0,0 +1,14 @@
+chrome.tabs.onUpdated.addListener((tabId, tab) => {
+  if (tab.url && tab.url.includes("youtube.com/watch")) {
+    const queryParmas = tab.url.split("?")[1];
+    const urlParams = new URLSearchParams(queryParmas);
+    setTimeout(
+      () =>
+        chrome.tabs.sendMessage(tabId, {
+          type: "NEW",
+          videoId: urlParams.get("v"),
+        }),
+      3000
+    );
+  }
+});
